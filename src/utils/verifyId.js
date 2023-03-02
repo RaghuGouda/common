9b347e54679c0aa6c14132a292520028f806c25b
Raghu <raghumn@testvagrant.com>
2023-03-02 21:55:04 +0530

    varify id

--- src/utils/verifyId.js
+++ src/utils/verifyId.js
@@ -4,12 +4,12 @@ const varifyId = async(req,res,next)=>{
     try {
         if (!ObjectId.isValid(req)){
             // return res.status(400).json({ error: 'invaild id' });
-            throw new Error('invaild id')
+            return res.status(400).json({ error: 'invaild id' });
         }
         next()     
     } catch (error) {
         error.status = 400
-        res.status(400).send({error})     
+        res.status(400).send('invaild id')     
     }
 }
 module.exports= varifyId
